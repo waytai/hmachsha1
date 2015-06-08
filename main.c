80510cfcbddb8784119fb9fba9b659d442997887
wayne <278972894@qq.com>
2015-06-08 15:02:15 +0800

    modify time to current time

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
     > Created Time: 2015年05月27日 星期三 16时46分27秒
  ************************************************************************/
 #include<stdio.h>
+#include<stdlib.h>
 #include <unistd.h>
 #include <time.h>
 #include <string.h>
@@ -38,20 +39,19 @@ char *encodeToHex(char *buff, const uint8_t *src, int len, int type) {
 
 
 
-int compute_code(const uint8_t *secret, int secretLen, unsigned long value) {
-  uint8_t val[8];
-  for (int i = 8; i--; value >>= 8) {
-    val[i] = value;
-  }
+int compute_code(const uint8_t *secret, int secretLen, char *value) {
+  //uint8_t val[8];
+  //for (int i = 8; i--; value >>= 8) {
+  //  val[i] = value;
+  //}
 
   uint8_t hash[SHA1_DIGEST_LENGTH];
   printf("--- secret is %s\n", secret);
-  printf("--- val is %s\n", val);
   printf("--- \n");
   //char val[20] = "47782269";
   //hmac_sha1(secret, secretLen, val, 8, hash, SHA1_DIGEST_LENGTH);
-  hmac_sha1(secret, secretLen, val, 8, hash, SHA1_DIGEST_LENGTH);
-  memset(val, 0, sizeof(val));
+  hmac_sha1(secret, secretLen, value, 8, hash, SHA1_DIGEST_LENGTH);
+  memset(value, 0, sizeof(value));
   int offset = hash[SHA1_DIGEST_LENGTH - 1] & 0xF;
   unsigned int truncatedHash = 0;
   for (int i = 0; i < 4; ++i) {
@@ -84,14 +84,20 @@ int main()
     //                                        sizeof(binary_secret));
     //printf("--------- %d\n", secretLen);
 
-    unsigned long value = get_timestamp();
+    //unsigned long value = get_timestamp();
     //value = 0;
-    value = 47782269;
-    printf("%lu\n", value); 
-    
+    char *value;
+
+    value = malloc(50);
+    unsigned long ul_value = get_timestamp();
+    snprintf(value, 50, "%d\n", ul_value);
+    printf("&&&&&%s\n", value);
+    //strcpy(value, "47782269");
+    //printf("%s\n", value); 
     int hs_code;
-    
+
     hs_code = compute_code(secret, secretLen, value);
+    free(value);
     printf("%d\n", hs_code);
     return 0;
 }
